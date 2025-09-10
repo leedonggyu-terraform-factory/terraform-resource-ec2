5f040c7cad543ed5232d3933bbbf90997dbb5358
leedonggyu <zkfmapf123@naver.com>
2025-09-10 14:52:10 +0900

    wip-97ec7717

--- security-group.tf
+++ security-group.tf
@@ -5,6 +5,14 @@ resource "aws_security_group" "ec2" {
     description = "${each.key}-ec2-sg"
     vpc_id = each.value.vpc_id
 
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+
     tags = {
         Name = "${each.key}-ec2-sg"
     }
