49380742acdc9213526b3b6ff4f0b9c5a834b8e4
leedonggyu <zkfmapf123@naver.com>
2025-11-01 14:29:38 +0900

    wip-dcc0548a

--- ec2.tf
+++ ec2.tf
@@ -22,6 +22,8 @@ resource "aws_instance" "ec2" {
     vpc_security_group_ids = [aws_security_group.ec2[each.key].id]
     iam_instance_profile = aws_iam_instance_profile.ec2_profile[each.key].name
 
+    public_ip = try(each.value.public_ip, false)
+
     user_data = each.value.user_data
 
     tags = {
