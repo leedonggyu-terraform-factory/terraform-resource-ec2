02bfaa57ba56b2e65edd3677bcb072ba5cdf74b1
leedonggyu <zkfmapf123@naver.com>
2025-12-25 12:53:25 +0900

    update keypair, public ip , private ip

--- ec2.tf
+++ ec2.tf
@@ -1,32 +1,33 @@
 data "aws_ami" "ec2" {
-    for_each = var.items
+  for_each = var.items
 
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["al2023-ami-2023.*-arm64"]
-    }
-    filter {
-        name = "architecture"
-        values = ["arm64"]
-    }
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-arm64"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
 }
 
 resource "aws_instance" "ec2" {
-    for_each = var.items
+  for_each = var.items
 
-    ami = try(each.value.ami , data.aws_ami.ec2[each.key].id)
-    instance_type = try(each.value.instance_type, "t4g.small")
-    subnet_id = each.value.subnet_id
-    vpc_security_group_ids = [aws_security_group.ec2[each.key].id]
-    iam_instance_profile = aws_iam_instance_profile.ec2_profile[each.key].name
+  ami                    = try(each.value.ami, data.aws_ami.ec2[each.key].id)
+  instance_type          = try(each.value.instance_type, "t4g.small")
+  subnet_id              = each.value.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2[each.key].id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile[each.key].name
 
-    associate_public_ip_address = try(each.value.associate_public_ip_address, false)
+  associate_public_ip_address = try(each.value.associate_public_ip_address, false)
 
-    user_data = each.value.user_data
+  user_data = each.value.user_data
+  key_name  = try(each.value.key_name, null)
 
-    tags = {
-        Name = "${each.key}-ec2"
-    }
+  tags = {
+    Name = "${each.key}-ec2"
+  }
 }
--- output.tf
+++ output.tf
@@ -4,6 +4,18 @@ output "ec2_id" {
   }
 }
 
+output "ec2_public_ip" {
+  value = {
+    for k, v in aws_instance.ec2 : k => v.public_ip
+  }
+}
+
+output "ec2_private_ip" {
+  value = {
+    for k, v in aws_instance.ec2 : k => v.private_ip
+  }
+}
+
 output "ec2_sg_ids" {
   value = {
     for k, v in aws_security_group.ec2 : k => v.id
